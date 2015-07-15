c8bdde51410593f1841416bc9d32f8b2af9f63d4
Sekadar Awan <sekadar.awan@gmail.com>
2015-07-16 03:45:54 +0700

    update

--- elpa/hyde-20150512.118/hyde-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "hyde" "20150512.118" "Major mode to help create and manage Jekyll blogs" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/tablist-20150602.827/tablist-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "tablist" "20150602.827" "Extended tabulated-list-mode"
-  '((emacs "24.3"))
-  :keywords
-  '("extensions" "lisp"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
