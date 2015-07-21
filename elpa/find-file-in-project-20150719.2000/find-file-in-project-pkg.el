7626959ba090cb6ca30aa5c5313aae08ec2b2d38
Sekadar Awan <sekadar.awan@gmail.com>
2015-07-21 21:37:57 +0700

    Add

---
+++ elpa/find-file-in-project-20150719.2000/find-file-in-project-pkg.el
@@ -0,0 +1,1 @@
+(define-package "find-file-in-project" "20150719.2000" "Find files in a project quickly." 'nil :url "https://github.com/technomancy/find-file-in-project" :keywords '("project" "convenience"))
