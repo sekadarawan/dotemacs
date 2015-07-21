4cfd605b6c5b0b9aa2c6a99dc9f259d19681c002
Sekadar Awan <sekadar.awan@gmail.com>
2015-07-21 21:31:43 +0700

    Add

---
+++ elpa/pydoc-20150525.1845/pydoc-autoloads.el
@@ -0,0 +1,25 @@
+;;; pydoc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "pydoc" "pydoc.el" (21934 22209 213168 870000))
+;;; Generated autoloads from pydoc.el
+
+(autoload 'pydoc "pydoc" "\
+Display pydoc information for NAME in a buffer named *pydoc*.
+
+\(fn NAME)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pydoc-pkg.el") (21934 22209 262760 860000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pydoc-autoloads.el ends here
