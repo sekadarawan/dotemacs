8ee2ec33e8023396a7213f212830cb214f4fcafe
Sekadar Awan <sekadar.awan@gmail.com>
2015-07-22 17:34:37 +0700

    Edit

---
+++ elpa/flymake-go-20150714.33/flymake-go-autoloads.el
@@ -0,0 +1,16 @@
+;;; flymake-go-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("flymake-go-pkg.el" "flymake-go.el") (21935
+;;;;;;  28399 5415 489000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flymake-go-autoloads.el ends here
