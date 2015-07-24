e28b351a12009f2328a5a72a07fa7e7de1ed4c01
Sekadar Awan <sekadar.awan@gmail.com>
2015-07-25 04:22:45 +0700

    Edit

--- init.el
+++ init.el
@@ -3,7 +3,10 @@
 	     '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
+(fset 'yes-or-no-p 'y-or-n-p)
+
 (require 'dired-details+)
+(require 'dired-rainbow)
 (require 'helm-config)
 (require 'powerline)
 (powerline-center-theme)
@@ -22,14 +25,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#282828" "#FAB1AB" "#D1FA71" "#FFA600" "#7b68ee" "#dc8cc3" "#96D9F1" "#F7F7F7"])
  '(c-default-style
    (quote
     ((c-mode . "linux")
      (java-mode . "java")
      (awk-mode . "awk")
      (other . "gnu"))))
+ '(coffee-mode-hook (quote (flymake-coffee-load)) t)
  '(company-backends
    (quote
     (company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-cmake company-capf
@@ -37,10 +39,11 @@
 		  company-oddmuse company-files company-dabbrev company-c-headers
 		  (company-jedi company-files)
 		  company-go company-ghc company-ghci)))
+ '(css-mode-hook (quote (flymake-css-load)) t)
  '(custom-enabled-themes (quote (smyx)))
  '(custom-safe-themes
    (quote
-    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" "8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" default)))
+    ("8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" default)))
  '(delete-by-moving-to-trash t)
  '(dired-dwim-target t)
  '(dired-hide-details-hide-symlink-targets nil)
@@ -49,24 +52,23 @@
  '(elpy-interactive-python-command "ipython")
  '(elpy-mode-hook (quote (subword-mode hl-line-mode)))
  '(elpy-rpc-backend "jedi")
- '(fci-rule-color "#151515")
- '(coffee-mode-hook (quote (flymake-coffee-load)) t)
- '(css-mode-hook (quote (flymake-css-load)) t)
- '(sccs-mode-hook (quote (flymake-sass-load)) t)
- '(sass-mode-hook (quote (flymake-sass-load)) t)
- '(js-mode-hook (quote (flymake-jshint-load flymake-jslint-load glasses-mode)) t)
  '(go-mode-hook (quote (flymake-go-load)) t)
  '(haskell-mode-hook
    (quote
     (flymake-haskell-multi-load flymake-hlint-load turn-on-haskell-indentation glasses-mode)) t)
- '(lua-mode-hook (quote (flymake-lua-load)) t)
  '(inhibit-startup-screen t)
+ '(js-mode-hook
+   (quote
+    (flymake-jshint-load flymake-jslint-load glasses-mode)) t)
  '(list-directory-brief-switches "--group-directories-first -lh")
  '(list-directory-verbose-switches "-alh")
+ '(lua-mode-hook (quote (flymake-lua-load)) t)
  '(major-mode (quote text-mode))
  '(prog-mode-hook (quote (company-mode nlinum-mode yas-minor-mode)) t)
  '(python-mode-hook (quote (elpy-mode)) t)
  '(ruby-mode-hook (quote (flymake-ruby-load glasses-mode)) t)
+ '(sass-mode-hook (quote (flymake-sass-load)))
+ '(sccs-mode-hook (quote (flymake-sass-load)) t)
  '(scroll-bar-mode nil)
  '(sh-basic-offset 2)
  '(sh-set-shell-hook (quote (flymake-shell-load)))
