b7399ec5a23bf6391f4344fdc6990cf0e9c2c458
Sekadar Awan <sekadar.awan@gmail.com>
2015-08-04 23:32:39 +0700

    Edit

--- elpa/simple-mpc-20150718.527/simple-mpc-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; simple-mpc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "simple-mpc" "simple-mpc.el" (21938 44602 872910
-;;;;;;  140000))
-;;; Generated autoloads from simple-mpc.el
-
-(autoload 'simple-mpc "simple-mpc" "\
-Starts simple-mpc.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("simple-mpc-current-playlist.el" "simple-mpc-mode.el"
-;;;;;;  "simple-mpc-pkg.el" "simple-mpc-query.el" "simple-mpc-utils.el"
-;;;;;;  "simple-mpc-vars.el") (21938 44602 975113 473000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; simple-mpc-autoloads.el ends here
--- elpa/simple-mpc-20150718.527/simple-mpc-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "simple-mpc" "20150718.527" "provides a simple interface to mpc" 'nil :keywords
-  '("multimedia" "mpd" "mpc"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
