b3eb31d95df28b675d666e2867cfceb8b86b5c69
Sekadar Awan <sekadar.awan@gmail.com>
2015-08-07 10:54:24 +0700

    Edit

--- init.el
+++ init.el
@@ -14,6 +14,23 @@
 (require 'flymake-rust)
 (require 'haml-mode)
 (require 'sass-mode)
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for web-mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-indent-style 2))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 
 (require 'slime-autoloads)
 (setq slime-lisp-implementations
