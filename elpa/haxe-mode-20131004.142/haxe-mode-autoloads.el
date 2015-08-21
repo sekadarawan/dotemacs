20938b4bb64ae91a4dd8be2d89255f8abef623a7
Sekadar Awan <sekadar.awan@gmail.com>
2015-08-22 05:56:51 +0700

    Edit

---
+++ elpa/haxe-mode-20131004.142/haxe-mode-autoloads.el
@@ -0,0 +1,15 @@
+;;; haxe-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("haxe-mode.el") (21975 36288 570564 691000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; haxe-mode-autoloads.el ends here
---
+++ elpa/haxe-mode-20131004.142/haxe-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "haxe-mode" "20131004.142" "An Emacs major mode for Haxe" 'nil :url "http://people.debian.org/~jps/misc/haxe-mode.el")
