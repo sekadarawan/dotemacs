51cad81e2bf1c00679d495f1b64d1f38bc486258
Sekadar Awan <sekadar.awan@gmail.com>
2015-08-22 00:17:54 +0700

    Edit

--- init.el
+++ init.el
@@ -29,7 +29,9 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
-  (setq web-mode-indent-style 2))
+  (setq web-mode-indent-style 2)
+  (yas-minor-mode t)
+  (company-mode t))
 (add-hook 'web-mode-hook 'my-web-mode-hook)
 
 (require 'slime-autoloads)
